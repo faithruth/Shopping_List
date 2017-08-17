6d2b85b3e9c4e7fd5edebcf0e6893d33309e9921
Imokol Faith ruth <faithruth27@gmil.com>
2017-08-17 14:50:37 +0300

    changes made

--- shopping_list.py
+++ shopping_list.py
@@ -1,9 +1,15 @@
 class ShoppingList(object):
-    def __init__(self, myList=[]):
-        self.items=myList
+    def __init__(self, myList):
+        self.myList=myList
 
-    def add_item(self, item):
+    def add_items(self, item):
+        self.myList.append(item)
+        return self.myList
+    def update_items(self, item):
         self.items.append(item)
+
+    
+
 
 
     
--- tests.py
+++ tests.py
@@ -7,8 +7,12 @@ class ShoppingListTestCase(unittest.TestCase):
         self.myList = ShoppingList()
 
     def test_items(self):
-        self.myList.items.append('perfume')
+        self.myList.items.insert(0,'perfume')
         self.assertEqual(self.myList.items, ['perfume'], msg='item not in list')
+
+    def test_add_items(self):
+        self.myList.add_items('lotion')
+        self.assertEqual(self.myList.add_items, ['lotion'], msg='item not added to list')
 
   
       
