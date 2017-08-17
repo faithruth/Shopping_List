0d8645a9932cb0032126feaac8dfc892c5392540
Imokol Faith ruth <faithruth27@gmil.com>
2017-08-17 11:41:36 +0300

    changes made

--- tests.py
+++ tests.py
@@ -10,9 +10,9 @@ class ShoppingListTestCase(unittest.TestCase):
         self.myList.items.append('perfume')
         self.assertEqual(self.myList.items, ['perfume'], msg='item not in list')
 
-    def test_add_items(self):
-        self.myList.add_item('honey')
-        self.assertEqual(self.myList.items, ['pefume', 'honey'], msg='item not added to list')
+    def test_add_item(self):
+        self.myList.items.append('honey')
+        self.assertEqual(self.myList.items, ['honey'], msg='item not added to list')
 
       
 
