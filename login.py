c51890e6f3fedcc0dd5a399c081fb8f0bef30d99
Imokol Faith ruth <faithruth27@gmil.com>
2017-08-17 16:23:26 +0300

    more modifications

--- login.py
+++
@@ -1,6 +0,0 @@
-class Login(object):
-    def __init__(self,UserName="Joab",Password="Joab@Andela.com"):
-        self.UserName=UserName
-        self.Password = Password
-    def loginUser(self,UserName,Password):
-        return("Welcome")
